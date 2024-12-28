e4a78c3dc624c33d036c048047ef664a72ae5586
varunTQ <varunsnair1997@gmail.com>
2024-12-28 23:47:35 +0530

    new project

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -72,25 +73,20 @@ function App(): React.JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+
+          <Text>Haiiiittttttttttttnnnnnnnnnnnttii</Text>
+          <Text>Haiiiiii</Text>
+
+<Text>Haiiiiii</Text>
+
+<Text>Haiiiiii</Text>
+
+<Text>Haiiiiii</Text>
+
+
         </View>
+     
       </ScrollView>
     </SafeAreaView>
   );
